0a1859a1484fce8559e89907f048914c55745ca5
Arthikhs <hsarthik5@gmail.com>
2026-08-06 22:34:00 +0530

    Initial commit

---
+++ p7abhi.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+public class p7abhi {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] arr = new int[n];
+        for(int i =0;i<arr.length;i++){
+            arr[i]=scan.nextInt();
+        }
+       int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                third = second;
+                second = num;
+            }
+            else if (num > third && num != first&& num != second) {
+                third =  num;
+            }
+        }
+
+        System.out.println("Second Largest: " + third);
+    }
+}
+   
+
