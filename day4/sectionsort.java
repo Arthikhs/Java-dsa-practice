85a9f30af9570b0a4b55247f6aeb36e9bd4c6dd3
Arthikhs <hsarthik5@gmail.com>
2026-09-04 18:10:51 +0530

    day4

--- day4/sectionsort.java
+++ day4/sectionsort.java
@@ -13,7 +13,7 @@ public class sectionsort {
     
     for(int i = 0;i<arr.length-1;i++){
         int minIndex = i;
-        for(int j = 0;j<arr.length-1-i;j++){
+        for(int j = i+1;j<arr.length;j++){
             if(arr[j]<arr[minIndex]){
                 minIndex = j;
             }
