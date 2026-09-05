b3d04e26d9e3f02556d7c2041ca2d5ff2350319c
Arthikhs <hsarthik5@gmail.com>
2026-09-05 22:38:02 +0530

    day5

---
+++ day5/sectionsort.java
@@ -0,0 +1,34 @@
+package day5;
+
+
+import java.util.Scanner;
+public class sectionsort {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int arr [] = new int[n];
+        for(int i = 0;i<arr.length;i++){
+            arr[i]= scan.nextInt();
+        }
+
+        scan.close();
+    
+    for(int i = 0;i<arr.length-1;i++){
+        int minIndex = i;
+        for(int j = i+1;j<arr.length;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i]= arr[minIndex];
+        arr[minIndex]= temp;
+
+    }
+    for(int i = 0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+    
+    }   
+}
+
