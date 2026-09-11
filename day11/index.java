2a89ffe286ce722fc778acde54591702bb36d066
Arthikhs <hsarthik5@gmail.com>
2026-09-11 19:43:42 +0530

    H

---
+++ day11/index.java
@@ -0,0 +1,17 @@
+package day11;
+import java.util.Scanner;
+public class index {
+
+public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int [] arr = new int[n];
+    for(int i =0;i<arr.length;i++){
+        arr[i]= scan.nextInt();
+    }
+    int first = arr[0];
+    for(int i = 0;i<arr.length-1;i++);
+    
+
+}
+}
