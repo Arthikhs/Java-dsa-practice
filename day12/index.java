9a8e5ae20d411e806f1bea6b5347c140dc1d5723
Arthikhs <hsarthik5@gmail.com>
2026-09-12 20:37:56 +0530

    H

---
+++ day12/index.java
@@ -0,0 +1,23 @@
+package day12;
+
+public class index {
+    
+}
+
+
+
+import java.util.Scanner;
+public class leftrotatearraybyone {
+public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int [] arr = new int[n];
+    for(int i =0;i<arr.length;i++){
+        arr[i]= scan.nextInt();
+    }
+    int first = arr[0];
+    for(int i = 0;i<arr.length-1;i++);
+    
+
+}
+}
